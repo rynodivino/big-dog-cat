72141ba318f23f5e861e963b50bbc1e69684f45c
ryan nichols <rynodivino@gmail.com>
2019-11-05 22:04:21 -0800

    testing

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,6 +11,26 @@ module.exports = {
   },
   plugins: [
     {
+    resolve: "gatsby-plugin-google-tagmanager",
+    options: {
+      id: "GTM-NSV4F2Q",
+
+      // Include GTM in development.
+      // Defaults to false meaning GTM will only be loaded in production.
+      includeInDevelopment: false,
+
+      // datalayer to be set before GTM is loaded
+      // should be an object or a function that is executed in the browser
+      // Defaults to null
+      defaultDataLayer: { platform: "gatsby" },
+
+      // Specify optional GTM environment details.
+//      gtmAuth: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_AUTH_STRING",
+ //     gtmPreview: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_PREVIEW_NAME",
+ //     dataLayerName: "YOUR_DATA_LAYER_NAME",
+    },
+  },
+    {
       resolve: "gatsby-plugin-tinacms",
       options: {
         plugins: ["gatsby-tinacms-git", "gatsby-tinacms-remark"],
